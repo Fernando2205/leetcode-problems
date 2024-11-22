6e459f237b2837d1e362b739665c383935178fa1
Fernando Palacios <deliopalaciosviafara@gmail.com>
2024-11-22 13:49:17 -0500

    Update test case description for addTwoNumbers in TestP2.rb

--- Problema 2/Ruby/TestP2.rb
+++ Problema 2/Ruby/TestP2.rb
@@ -8,7 +8,7 @@ class TestAddTwoNumbers < Test::Unit::TestCase
       {l1: [2,4,3], l2: [5,6,4], expected: [7,0,8], description: '342 + 465 = 807'},
       {l1: [0], l2: [0], expected: [0], description: '0 + 0 = 0'},
       {l1: [9,9,9,9,9,9,9], l2: [9,9,9,9], expected: [8,9,9,9,0,0,0,1], description: '9999999 + 9999'},
-      {l1: [1,8], l2: [0], expected: [1,8], description: '81 + 0'},
+      {l1: [1,8], l2: [0], expected: [1,8], description: '81 + 0 = 81'},
       {l1: [5], l2: [5], expected: [0,1], description: '5 + 5 = 10'}
     ]
   end
